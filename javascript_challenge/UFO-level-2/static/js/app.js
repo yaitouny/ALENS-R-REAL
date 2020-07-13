09711fd17228df559f97ecfaa4a54101b90df731
Yasmine Aitouny <aitouny680@gmail.com>
2020-07-12 22:17:45 -0500

    fix cities filtering bug

--- javascript_challenge/UFO-level-2/static/js/app.js
+++ javascript_challenge/UFO-level-2/static/js/app.js
@@ -1,9 +1,69 @@
 // from data.js
 var tableData = data;
-console.log(tableData);
+// use d3 to select the table
+var table = d3.select(".table");
+
+// use d3 to select the table body
+var body = table.select("tbody");
+
+function init() {
+  const cities = tableData.map((row) => row.city);
+  const uniqueCities = Array.from(new Set(cities)).sort();
+
+  uniqueCities.forEach((city) => {
+    $("#filteredCity").append(
+      $("<option>", {
+        value: city,
+        text: city,
+      })
+    );
+  });
+
+  const states = tableData.map((row) => row.state);
+  const uniqueStates = Array.from(new Set(states)).sort();
+
+  uniqueStates.forEach((state) => {
+    $("#filteredState").append(
+      $("<option>", {
+        value: state,
+        text: state,
+      })
+    );
+  });
+
+  const shapes = tableData.map((row) => row.shape);
+  const uShapes = Array.from(new Set(shapes)).sort();
+
+  uShapes.forEach((shape) => {
+    $("#filteredShape").append(
+      $("<option>", {
+        value: shape,
+        text: shape,
+      })
+    );
+  });
+  // tableData.forEach((dataRow) => {
+  //   $("filteredState").append(
+  //     $("<option>", {
+  //       value: dataRow.state,
+  //       value: dataRow.state,
+  //     })
+  //   );
+  // });
+
+  tableData.forEach(function (list) {
+    let newRow = body.append("tr");
+    Object.entries(list).forEach(function ([key, value]) {
+      newRow.append("td").text(value);
+    });
+  });
+}
+
+init();
 
 // Select the button
 var filterButton = d3.select("#filter-btn");
+
 // Select the form
 var form = d3.select("#form");
 
@@ -21,34 +81,40 @@ function findValue() {
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
-  // Print the value to the console
-  console.log(inputValue);
-  console.log(tableData);
-
   // Filter by datetime
   var filteredData = tableData.filter((x) => x.datetime === inputValue);
+  console.log(filteredData);
 
-  var cityFilter = d3.select("#filteredCity");
-  var countryFilter = d3.select("#filteredCountry");
-  var shapeFilter = d3.select("#filteredShape");
+  // var cityFilter = d3.select("filteredCity");
+  // filteredData = filteredData.map((x) => x.city === cityFilter);
 
-  cityFilter = filteredData.filter((x) => x.city === cityFilter);
-  console.log(cityFilter);
+  filteredData.forEach((dataRow) => {
+    console.log(dataRow.city);
+    $("#filteredCity").append(
+      $("<option>", {
+        value: dataRow.city,
+        text: dataRow.city,
+      })
+    );
+  });
 
-  if (countryFilter != "All") {
-    countryFilter = filteredData.filter((x) => x.country === countryFilter);
-  }
+  filteredData.forEach((dataRow) => {
+    $("#filteredShape").append(
+      $("<option", {
+        value: dataRow.shape,
+        text: dataRow.shape,
+      })
+    );
+  });
 
-  if (shapeFilter != "All") {
-    shapeFilter = filteredData.filter((x) => x.shape === shapeFilter);
-  }
-
-  // use d3 to select the table
-  var table = d3.select(".table");
-
-  // use d3 to select the table body
-  var body = table.select("tbody");
-
+  filteredData.forEach((dataRow) => {
+    $("#filteredState").append(
+      $("<option", {
+        value: dataRow.state,
+        text: dataRow.state,
+      })
+    );
+  });
   // Clear existing data
   table.select("tbody").html("");
   filteredData.forEach(function (list) {
